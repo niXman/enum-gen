9fab973a281ccd75cb846d8db45856c1ba6a719f
niXman <i.nixman@gmail.com>
2013-08-12 12:40:18 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,88 @@
+
+#include "enum-gen.hpp"
+
+#include <iostream>
+#include <sstream>
+
+/***************************************************************************/
+
+DECLARE_ENUM_CLASS(
+   myenum1, std::uint8_t,
+   (member1)
+   (member2)
+   (member3)
+);
+
+DECLARE_ENUM_CLASS(
+   myenum2, std::uint8_t,
+   (member4)
+   (member5)
+   (member6)
+);
+
+DECLARE_ENUM(
+   myenum3,
+   (member7)
+   (member8)
+   (member9)
+);
+
+DECLARE_ENUM(
+   myenum4,
+   (member10)
+   (member11)
+   (member12)
+);
+
+/***************************************************************************/
+
+void test_out() {
+   std::stringstream s1;
+   myenum1 e1 = myenum1::member2;
+   s1 << e1;
+   assert(0 == strcmp(s1.str().c_str(), "myenum1::member2"));
+   
+   std::stringstream s2;
+   myenum2 e2 = myenum2::member5;
+   s2 << e2;
+   assert(0 == strcmp(s2.str().c_str(), "myenum2::member5"));
+   
+   std::stringstream s3;
+   myenum3 e3 = myenum3::member8;
+   s3 << e3;
+   assert(0 == strcmp(s3.str().c_str(), "myenum3::member8"));
+   
+   std::stringstream s4;
+   myenum4 e4 = myenum4::member11;
+   s4 << e4;
+   assert(0 == strcmp(s4.str().c_str(), "myenum4::member11"));
+   
+   std::cout << "test_out() PASSED" << std::endl;
+}
+
+void test_cast_to_char() {
+   assert(0 == strcmp("myenum1::member2", enum_cast(myenum1::member2)));
+   assert(0 == strcmp("myenum2::member5", enum_cast(myenum2::member5)));
+   assert(0 == strcmp("myenum3::member8", enum_cast(myenum3::member8)));
+   assert(0 == strcmp("myenum4::member11", enum_cast(myenum4::member11)));
+   
+   std::cout << "test_cast_to_char() PASSED" << std::endl;
+}
+
+void test_cast_from_char() {
+   assert(myenum1::member2 == enum_cast<myenum1>("myenum1::member2"));
+   assert(myenum2::member5 == enum_cast<myenum2>("myenum2::member5"));
+   assert(myenum3::member8 == enum_cast<myenum3>("myenum3::member8"));
+   assert(myenum4::member11 == enum_cast<myenum4>("myenum4::member11"));
+   std::cout << "test_cast_from_char() PASSED" << std::endl;
+}
+
+/***************************************************************************/
+
+int main() {
+   test_out();
+   test_cast_to_char();
+   test_cast_from_char();
+}
+
+/***************************************************************************/
