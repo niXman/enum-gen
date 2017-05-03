0fad71f18d981e2b6c318b3f367277b9fd40ea9b
niXman <i.nixman@autistici.org>
2017-05-03 11:01:40 +0300

    const_iterator added

--- enum-gen.hpp
+++ enum-gen.hpp
@@ -53,9 +53,9 @@
 /****************************************************************************/
 
 #define ENUM_GEN_WRAP_SEQUENCE_X(...) \
-	((__VA_ARGS__)) ENUM_GEN_WRAP_SEQUENCE_Y
+    ((__VA_ARGS__)) ENUM_GEN_WRAP_SEQUENCE_Y
 #define ENUM_GEN_WRAP_SEQUENCE_Y(...) \
-	((__VA_ARGS__)) ENUM_GEN_WRAP_SEQUENCE_X
+    ((__VA_ARGS__)) ENUM_GEN_WRAP_SEQUENCE_X
 
 #define ENUM_GEN_WRAP_SEQUENCE_X0
 #define ENUM_GEN_WRAP_SEQUENCE_Y0
@@ -63,150 +63,152 @@
 /****************************************************************************/
 
 #define ENUM_GEN_ADAPT_ENUM_GENERATE_CASES(unused, data, idx, elem) \
-	case data::BOOST_PP_TUPLE_ELEM(0, elem): \
-		return enum_info<data>::values[idx].name+offset \
-	;
+    case data::BOOST_PP_TUPLE_ELEM(0, elem): \
+        return enum_info<data>::values[idx].name+offset \
+    ;
 
 #define ENUM_GEN_ADAPT_ENUM_GENERATE_VALUES(unused, data, idx, elem) \
-	{   BOOST_PP_STRINGIZE(data::BOOST_PP_TUPLE_ELEM(0, elem)) \
-		,data::BOOST_PP_TUPLE_ELEM(0, elem) \
-		,static_cast<enum_info<data>::underlying_type>(data::BOOST_PP_TUPLE_ELEM(0, elem)) \
-	},
+    {   BOOST_PP_STRINGIZE(data::BOOST_PP_TUPLE_ELEM(0, elem)) \
+        ,data::BOOST_PP_TUPLE_ELEM(0, elem) \
+        ,static_cast<enum_info<data>::underlying_type>(data::BOOST_PP_TUPLE_ELEM(0, elem)) \
+    },
 
 #define ENUM_GEN_ADAPT_ENUM_IMPL(name_, seq) \
-	template<typename> \
-	struct enum_info; \
-	\
-	template<> \
-	struct enum_info<name_> { \
-		using underlying_type = std::underlying_type<name_>::type; \
-		struct value_type { \
-			const char *name; \
-			const name_ value; \
-			const underlying_type ivalue; \
-		}; \
-		static const value_type values[]; \
-	}; \
-	const \
-	enum_info<name_>::value_type \
-	enum_info<name_>::values[] = { \
-		BOOST_PP_SEQ_FOR_EACH_I( \
-			 ENUM_GEN_ADAPT_ENUM_GENERATE_VALUES \
-			,name_ \
-			,seq \
-		) \
-	}; \
-	\
-	inline const char* enum_cast(const name_ e, const bool full_name = true) { \
-		const std::size_t offset = (true == full_name ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
-		switch ( e ) { \
-			BOOST_PP_SEQ_FOR_EACH_I( \
-				 ENUM_GEN_ADAPT_ENUM_GENERATE_CASES \
-				,name_ \
-				,seq \
-			) \
-		} \
-		assert("bad enum value #1" == 0); \
-	} \
-	template<typename E> \
-	E enum_cast(const char *); \
-	template<> \
-	inline name_ enum_cast(const char *str) { \
-		const std::size_t offset = (0 != std::strchr(str, ':') ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
-		for ( const auto &it: enum_info<name_>::values ) { \
-			if ( 0 == std::strcmp(it.name+offset, str) ) \
-				return it.value; \
-		} \
-		assert("bad enum value #2" == 0); \
-	} \
-	\
-	template<typename E> \
-	bool has_member(const char *); \
-	template<> \
-	inline bool has_member<name_>(const char *str) { \
-		const std::size_t offset = (0 != std::strchr(str, ':') ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
-		for ( const auto &it: enum_info<name_>::values ) { \
-			if ( 0 == std::strcmp(it.name+offset, str) ) \
-				return true; \
-		} \
-		return false; \
-	} \
-	\
-	std::ostream& operator<< (std::ostream &os, const name_ e) { \
-		return (os << enum_cast(e)); \
-	} \
-	\
-	inline constexpr enum_info<name_>::underlying_type operator| ( \
-		 const name_ l, const name_ r \
-	) { \
-		return \
-			static_cast<enum_info<name_>::underlying_type>(l) \
-			| \
-			static_cast<enum_info<name_>::underlying_type>(r) \
-		; \
-	} \
-	inline constexpr enum_info<name_>::underlying_type operator& ( \
-		 const name_ l, const enum_info<name_>::underlying_type r \
-	) { \
-		return static_cast<enum_info<name_>::underlying_type>(l) & r; \
-	} \
-	inline constexpr enum_info<name_>::underlying_type operator& ( \
-		 const enum_info<name_>::underlying_type l, const name_ r \
-	) { \
-		return l & static_cast<enum_info<name_>::underlying_type>(r); \
-	}
+    template<typename> \
+    struct enum_info; \
+    \
+    template<> \
+    struct enum_info<name_> { \
+        using underlying_type = std::underlying_type<name_>::type; \
+        struct value_type { \
+            const char *name; \
+            const name_ value; \
+            const underlying_type ivalue; \
+        }; \
+        static const value_type values[]; \
+        \
+        using const_iterator = value_type const*; \
+        static const_iterator begin() { return &values[0]; } \
+        static const_iterator end() { return &values[BOOST_PP_SEQ_SIZE(seq)]; } \
+    }; \
+    const enum_info<name_>::value_type enum_info<name_>::values[] = { \
+        BOOST_PP_SEQ_FOR_EACH_I( \
+            ENUM_GEN_ADAPT_ENUM_GENERATE_VALUES \
+            ,name_ \
+            ,seq \
+        ) \
+    }; \
+    \
+    inline const char* enum_cast(const name_ e, const bool full_name = true) { \
+        const std::size_t offset = (true == full_name ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
+        switch ( e ) { \
+            BOOST_PP_SEQ_FOR_EACH_I( \
+                ENUM_GEN_ADAPT_ENUM_GENERATE_CASES \
+                ,name_ \
+                ,seq \
+            ) \
+        } \
+        assert("bad enum value #1" == 0); \
+    } \
+    template<typename E> \
+    E enum_cast(const char *); \
+    template<> \
+    inline name_ enum_cast(const char *str) { \
+        const std::size_t offset = (0 != std::strchr(str, ':') ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
+        for ( const auto &it: enum_info<name_>::values ) { \
+            if ( 0 == std::strcmp(it.name+offset, str) ) \
+                return it.value; \
+        } \
+        assert("bad enum value #2" == 0); \
+    } \
+    \
+    template<typename E> \
+    bool has_member(const char *); \
+    template<> \
+    inline bool has_member<name_>(const char *str) { \
+        const std::size_t offset = (0 != std::strchr(str, ':') ? 0 : sizeof(BOOST_PP_STRINGIZE(name_::))-1); \
+        for ( const auto &it: enum_info<name_>::values ) { \
+            if ( 0 == std::strcmp(it.name+offset, str) ) \
+                return true; \
+        } \
+        return false; \
+    } \
+    \
+    std::ostream& operator<< (std::ostream &os, const name_ e) { \
+        return (os << enum_cast(e)); \
+    } \
+    \
+    inline constexpr enum_info<name_>::underlying_type operator| ( \
+        const name_ l, const name_ r \
+    ) { \
+        return \
+            static_cast<enum_info<name_>::underlying_type>(l) \
+            | \
+            static_cast<enum_info<name_>::underlying_type>(r) \
+        ; \
+    } \
+    inline constexpr enum_info<name_>::underlying_type operator& ( \
+        const name_ l, const enum_info<name_>::underlying_type r \
+    ) { \
+        return static_cast<enum_info<name_>::underlying_type>(l) & r; \
+    } \
+    inline constexpr enum_info<name_>::underlying_type operator& ( \
+        const enum_info<name_>::underlying_type l, const name_ r \
+    ) { \
+        return l & static_cast<enum_info<name_>::underlying_type>(r); \
+    }
 
 #define ENUM_GEN_ADAPT_ENUM(name, seq) \
-	ENUM_GEN_ADAPT_ENUM_IMPL( \
-		 name \
-		,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
-	)
+    ENUM_GEN_ADAPT_ENUM_IMPL( \
+        name \
+        ,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
+    )
 
 /****************************************************************************/
 
 #define ENUM_GEN_DECLARE_ENUM_MEMBERS(unused, data, idx, elem) \
-	BOOST_PP_COMMA_IF(idx) BOOST_PP_IF( \
-		 BOOST_PP_EQUAL(2, BOOST_PP_TUPLE_SIZE(elem)) \
-		,BOOST_PP_TUPLE_ELEM(0, elem)=BOOST_PP_TUPLE_ELEM(1, elem) /* member = value */ \
-		,BOOST_PP_TUPLE_ELEM(0, elem) /* member */ \
-	)
+    BOOST_PP_COMMA_IF(idx) BOOST_PP_IF( \
+        BOOST_PP_EQUAL(2, BOOST_PP_TUPLE_SIZE(elem)) \
+        ,BOOST_PP_TUPLE_ELEM(0, elem)=BOOST_PP_TUPLE_ELEM(1, elem) /* member = value */ \
+        ,BOOST_PP_TUPLE_ELEM(0, elem) /* member */ \
+    )
 
 /****************************************************************************/
 
 #define ENUM_GEN_DECLARE_ENUM_IMPL(name, seq) \
-	enum name { \
-	BOOST_PP_SEQ_FOR_EACH_I( \
-			 ENUM_GEN_DECLARE_ENUM_MEMBERS \
-			,~ \
-			,seq \
-		) \
-	}; \
-	ENUM_GEN_ADAPT_ENUM_IMPL(name, seq)
+    enum name { \
+    BOOST_PP_SEQ_FOR_EACH_I( \
+            ENUM_GEN_DECLARE_ENUM_MEMBERS \
+            ,~ \
+            ,seq \
+        ) \
+    }; \
+    ENUM_GEN_ADAPT_ENUM_IMPL(name, seq)
 
 #define ENUM_GEN_DECLARE_ENUM(name, seq) \
-	ENUM_GEN_DECLARE_ENUM_IMPL( \
-		 name \
-		,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
-	)
+    ENUM_GEN_DECLARE_ENUM_IMPL( \
+        name \
+        ,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
+    )
 
 /****************************************************************************/
 
 #define ENUM_GEN_DECLARE_ENUM_CLASS_IMPL(name, type, seq) \
-	enum class name : type { \
-	BOOST_PP_SEQ_FOR_EACH_I( \
-			 ENUM_GEN_DECLARE_ENUM_MEMBERS \
-			,~ \
-			,seq \
-		) \
-	}; \
-	ENUM_GEN_ADAPT_ENUM_IMPL(name, seq)
+    enum class name : type { \
+    BOOST_PP_SEQ_FOR_EACH_I( \
+            ENUM_GEN_DECLARE_ENUM_MEMBERS \
+            ,~ \
+            ,seq \
+        ) \
+    }; \
+    ENUM_GEN_ADAPT_ENUM_IMPL(name, seq)
 
 #define ENUM_GEN_DECLARE_ENUM_CLASS(name, type, seq) \
-	ENUM_GEN_DECLARE_ENUM_CLASS_IMPL( \
-		 name \
-		,type \
-		,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
-	)
+    ENUM_GEN_DECLARE_ENUM_CLASS_IMPL( \
+        name \
+        ,type \
+        ,BOOST_PP_CAT(ENUM_GEN_WRAP_SEQUENCE_X seq, 0) \
+    )
 
 /****************************************************************************/
 
--- main.cpp
+++ main.cpp
@@ -41,201 +41,235 @@
 /***************************************************************************/
 
 #define MY_ASSERT(...) \
-	if ( !(__VA_ARGS__) ) { \
-		std::cerr << "expression error(" __FILE__ ":" BOOST_PP_STRINGIZE(__LINE__) "): \"" #__VA_ARGS__ "\"" << std::endl; \
-		std::abort(); \
-	}
+    if ( !(__VA_ARGS__) ) { \
+        std::cerr << "expression error(" __FILE__ ":" BOOST_PP_STRINGIZE(__LINE__) "): \"" #__VA_ARGS__ "\"" << std::endl; \
+        std::abort(); \
+    }
 
 /***************************************************************************/
 
 enum class myenum0: std::uint8_t {
-	 member0
-	,member1
-	,member2
+    member0
+    ,member1
+    ,member2
 };
 ENUM_GEN_ADAPT_ENUM(
-	 myenum0
-	,
-	(member0)
-	(member1)
-	(member2)
+    myenum0
+    ,
+    (member0)
+    (member1)
+    (member2)
 )
 
 ENUM_GEN_DECLARE_ENUM_CLASS(
-	 myenum1
-	,std::uint8_t
-	,
-	(member1, 1<<0)
-	(member2, 1<<1)
-	(member3, 1<<2)
-	(member4, 1<<3)
+    myenum1
+    ,std::uint8_t
+    ,
+    (member1, 1<<0)
+    (member2, 1<<1)
+    (member3, 1<<2)
+    (member4, 1<<4)
 );
 
 ENUM_GEN_DECLARE_ENUM_CLASS(
-	 myenum2
-	,std::uint8_t
-	,
-	(member4)
-	(member5,0x10)
-	(member6)
+    myenum2
+    ,std::uint8_t
+    ,
+    (member4)
+    (member5,0x10)
+    (member6)
 );
 
 ENUM_GEN_DECLARE_ENUM_CLASS(
-	 myenum3
-	,std::uint32_t
-	,
-	(member7)
-	(member8)
-	(member9)
+    myenum3
+    ,std::uint32_t
+    ,
+    (member7)
+    (member8)
+    (member9)
 );
 
 ENUM_GEN_DECLARE_ENUM_CLASS(
-	 myenum4
-	,std::uint64_t
-	,
-	(member10)
-	(member11)
-	(member12)
+    myenum4
+    ,std::uint64_t
+    ,
+    (member10)
+    (member11)
+    (member12)
 );
 
 ENUM_GEN_DECLARE_ENUM(
-	 myenum5
-	,
-	(member13)
-	(member14)
-	(member15)
+    myenum5
+    ,
+    (member13)
+    (member14)
+    (member15)
 );
 
 /***************************************************************************/
 
 void test_for_each() {
-	int idx = 0;
-	for ( const auto &it: enum_info<myenum0>::values ) {
-		switch ( idx ) {
-			case 0: MY_ASSERT(
-				0         == strcmp(it.name, "myenum0::member0") &&
-				0         == strcmp("myenum0::member0", enum_cast(it.value)) &&
-				it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
-				0         == it.ivalue
-			);
-			break;
-			case 1: MY_ASSERT(
-				0         == strcmp(it.name, "myenum0::member1") &&
-				0         == strcmp("myenum0::member1", enum_cast(it.value)) &&
-				it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
-				1         == it.ivalue
-			);
-			break;
-			case 2: MY_ASSERT(
-				0         == strcmp(it.name, "myenum0::member2") &&
-				0         == strcmp("myenum0::member2", enum_cast(it.value)) &&
-				it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
-				2         == it.ivalue
-			);
-			break;
-			default: MY_ASSERT(0)
-		}
-		++idx;
-	}
+    int idx = 0;
+    for ( const auto &it: enum_info<myenum0>::values ) {
+        switch ( idx ) {
+            case 0: MY_ASSERT(
+                0         == strcmp(it.name, "myenum0::member0") &&
+                0         == strcmp("myenum0::member0", enum_cast(it.value)) &&
+                it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
+                0         == it.ivalue
+            );
+            break;
+            case 1: MY_ASSERT(
+                0         == strcmp(it.name, "myenum0::member1") &&
+                0         == strcmp("myenum0::member1", enum_cast(it.value)) &&
+                it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
+                1         == it.ivalue
+            );
+            break;
+            case 2: MY_ASSERT(
+                0         == strcmp(it.name, "myenum0::member2") &&
+                0         == strcmp("myenum0::member2", enum_cast(it.value)) &&
+                it.ivalue == static_cast<enum_info<myenum0>::underlying_type>(it.value) &&
+                2         == it.ivalue
+            );
+            break;
+            default: MY_ASSERT(0)
+        }
+        ++idx;
+    }
 
-	std::cout << "test_for_each() PASSED" << std::endl;
+    std::cout << "test_for_each() PASSED" << std::endl;
+}
+
+void test_iterators() {
+    int idx = 0;
+    for ( auto it = enum_info<myenum0>::begin(); it != enum_info<myenum0>::end(); ++it ) {
+        switch ( idx ) {
+            case 0: MY_ASSERT(
+                0         == strcmp(it->name, "myenum0::member0") &&
+                0         == strcmp("myenum0::member0", enum_cast(it->value)) &&
+                it->ivalue == static_cast<enum_info<myenum0>::underlying_type>(it->value) &&
+                0         == it->ivalue
+            );
+            break;
+            case 1: MY_ASSERT(
+                0         == strcmp(it->name, "myenum0::member1") &&
+                0         == strcmp("myenum0::member1", enum_cast(it->value)) &&
+                it->ivalue == static_cast<enum_info<myenum0>::underlying_type>(it->value) &&
+                1         == it->ivalue
+            );
+            break;
+            case 2: MY_ASSERT(
+                0         == strcmp(it->name, "myenum0::member2") &&
+                0         == strcmp("myenum0::member2", enum_cast(it->value)) &&
+                it->ivalue == static_cast<enum_info<myenum0>::underlying_type>(it->value) &&
+                2         == it->ivalue
+            );
+            break;
+            default: MY_ASSERT(0)
+        }
+        ++idx;
+    }
+
+    std::cout << "test_iterators() PASSED" << std::endl;
 }
 
 void test_out() {
-	std::stringstream s1;
-	myenum1 e1 = myenum1::member2;
-	s1 << e1;
-	MY_ASSERT(0 == strcmp(s1.str().c_str(), "myenum1::member2"));
+    std::stringstream s1;
+    myenum1 e1 = myenum1::member2;
+    s1 << e1;
+    MY_ASSERT(0 == strcmp(s1.str().c_str(), "myenum1::member2"));
 
-	std::stringstream s2;
-	myenum2 e2 = myenum2::member5;
-	s2 << e2;
-	MY_ASSERT(0 == strcmp(s2.str().c_str(), "myenum2::member5"));
+    std::stringstream s2;
+    myenum2 e2 = myenum2::member5;
+    s2 << e2;
+    MY_ASSERT(0 == strcmp(s2.str().c_str(), "myenum2::member5"));
 
-	std::stringstream s3;
-	myenum3 e3 = myenum3::member8;
-	s3 << e3;
-	MY_ASSERT(0 == strcmp(s3.str().c_str(), "myenum3::member8"));
+    std::stringstream s3;
+    myenum3 e3 = myenum3::member8;
+    s3 << e3;
+    MY_ASSERT(0 == strcmp(s3.str().c_str(), "myenum3::member8"));
 
-	std::stringstream s4;
-	myenum4 e4 = myenum4::member11;
-	s4 << e4;
-	MY_ASSERT(0 == strcmp(s4.str().c_str(), "myenum4::member11"));
+    std::stringstream s4;
+    myenum4 e4 = myenum4::member11;
+    s4 << e4;
+    MY_ASSERT(0 == strcmp(s4.str().c_str(), "myenum4::member11"));
 
-	std::stringstream s5;
-	myenum5 e5 = myenum5::member13;
-	s5 << e5;
-	MY_ASSERT(0 == strcmp(s5.str().c_str(), "myenum5::member13"));
+    std::stringstream s5;
+    myenum5 e5 = myenum5::member13;
+    s5 << e5;
+    MY_ASSERT(0 == strcmp(s5.str().c_str(), "myenum5::member13"));
 
-	std::cout << "test_out() PASSED" << std::endl;
+    std::cout << "test_out() PASSED" << std::endl;
 }
 
 void test_cast_to_char() {
-	MY_ASSERT(0 == strcmp("myenum1::member2", enum_cast(myenum1::member2)));
-	MY_ASSERT(0 == strcmp("myenum2::member5", enum_cast(myenum2::member5)));
-	MY_ASSERT(0 == strcmp("myenum3::member8", enum_cast(myenum3::member8)));
-	MY_ASSERT(0 == strcmp("myenum4::member11", enum_cast(myenum4::member11)));
-	MY_ASSERT(0 == strcmp("myenum5::member13", enum_cast(myenum5::member13)));
+    MY_ASSERT(0 == strcmp("myenum1::member2", enum_cast(myenum1::member2)));
+    MY_ASSERT(0 == strcmp("myenum2::member5", enum_cast(myenum2::member5)));
+    MY_ASSERT(0 == strcmp("myenum3::member8", enum_cast(myenum3::member8)));
+    MY_ASSERT(0 == strcmp("myenum4::member11", enum_cast(myenum4::member11)));
+    MY_ASSERT(0 == strcmp("myenum5::member13", enum_cast(myenum5::member13)));
 
-	MY_ASSERT(0 == strcmp("member2", enum_cast(myenum1::member2, false)));
-	MY_ASSERT(0 == strcmp("member5", enum_cast(myenum2::member5, false)));
-	MY_ASSERT(0 == strcmp("member8", enum_cast(myenum3::member8, false)));
-	MY_ASSERT(0 == strcmp("member11", enum_cast(myenum4::member11, false)));
-	MY_ASSERT(0 == strcmp("member13", enum_cast(myenum5::member13, false)));
+    MY_ASSERT(0 == strcmp("member2", enum_cast(myenum1::member2, false)));
+    MY_ASSERT(0 == strcmp("member5", enum_cast(myenum2::member5, false)));
+    MY_ASSERT(0 == strcmp("member8", enum_cast(myenum3::member8, false)));
+    MY_ASSERT(0 == strcmp("member11", enum_cast(myenum4::member11, false)));
+    MY_ASSERT(0 == strcmp("member13", enum_cast(myenum5::member13, false)));
 
-	std::cout << "test_cast_to_char() PASSED" << std::endl;
+    std::cout << "test_cast_to_char() PASSED" << std::endl;
 }
 
 void test_cast_from_char() {
-	MY_ASSERT(myenum1::member2 == enum_cast<myenum1>("myenum1::member2"));
-	MY_ASSERT(myenum1::member2 == enum_cast<myenum1>("member2"));
-	MY_ASSERT(myenum2::member5 == enum_cast<myenum2>("myenum2::member5"));
-	MY_ASSERT(myenum2::member5 == enum_cast<myenum2>("member5"));
-	MY_ASSERT(myenum3::member8 == enum_cast<myenum3>("myenum3::member8"));
-	MY_ASSERT(myenum3::member8 == enum_cast<myenum3>("member8"));
-	MY_ASSERT(myenum4::member11 == enum_cast<myenum4>("myenum4::member11"));
-	MY_ASSERT(myenum4::member11 == enum_cast<myenum4>("member11"));
-	MY_ASSERT(myenum5::member13 == enum_cast<myenum5>("myenum5::member13"));
-	MY_ASSERT(myenum5::member13 == enum_cast<myenum5>("member13"));
+    MY_ASSERT(myenum1::member2 == enum_cast<myenum1>("myenum1::member2"));
+    MY_ASSERT(myenum1::member2 == enum_cast<myenum1>("member2"));
+    MY_ASSERT(myenum2::member5 == enum_cast<myenum2>("myenum2::member5"));
+    MY_ASSERT(myenum2::member5 == enum_cast<myenum2>("member5"));
+    MY_ASSERT(myenum3::member8 == enum_cast<myenum3>("myenum3::member8"));
+    MY_ASSERT(myenum3::member8 == enum_cast<myenum3>("member8"));
+    MY_ASSERT(myenum4::member11 == enum_cast<myenum4>("myenum4::member11"));
+    MY_ASSERT(myenum4::member11 == enum_cast<myenum4>("member11"));
+    MY_ASSERT(myenum5::member13 == enum_cast<myenum5>("myenum5::member13"));
+    MY_ASSERT(myenum5::member13 == enum_cast<myenum5>("member13"));
 
-	std::cout << "test_cast_from_char() PASSED" << std::endl;
+    std::cout << "test_cast_from_char() PASSED" << std::endl;
 }
 
 void test_operators() {
-	static_assert(
-		 std::is_same<std::underlying_type<myenum1>::type, std::uint8_t>::value
-		 && std::is_same<decltype(myenum1::member1 | myenum1::member2), std::underlying_type<myenum1>::type>::value
-		,"type error!"
-	);
+    static_assert(
+        std::is_same<std::underlying_type<myenum1>::type, std::uint8_t>::value
+        && std::is_same<decltype(myenum1::member1 | myenum1::member2), std::underlying_type<myenum1>::type>::value
+        ,"type error!"
+    );
 
-	constexpr auto v0 = myenum1::member1 | myenum1::member2;
-	static_assert(v0 == 3, "'operator|' error!");
+    constexpr auto v0 = myenum1::member1 | myenum1::member2;
+    static_assert(v0 == 3, "'operator|' error!");
 
-	constexpr auto v1 = (v0 & myenum1::member2);
-	static_assert(
-		 std::is_same<std::remove_const<decltype(v1)>::type, std::uint8_t>::value && v1 == (std::uint8_t)2
-		,"'operator&' error!"
-	);
+    constexpr auto v1 = (v0 & myenum1::member2);
+    static_assert(
+        std::is_same<std::remove_const<decltype(v1)>::type, std::uint8_t>::value && v1 == (std::uint8_t)2
+        ,"'operator&' error!"
+    );
 
-	constexpr auto v2 = (v0 & myenum1::member3);
-	static_assert(
-		 std::is_same<std::remove_const<decltype(v1)>::type, std::uint8_t>::value && v2 == (std::uint8_t)0
-		,"'operator&' error!"
-	);
+    constexpr auto v2 = (v0 & myenum1::member3);
+    static_assert(
+        std::is_same<std::remove_const<decltype(v1)>::type, std::uint8_t>::value && v2 == (std::uint8_t)0
+        ,"'operator&' error!"
+    );
 
-	std::cout << "test_operators() PASSED" << std::endl;
+    std::cout << "test_operators() PASSED" << std::endl;
 }
 
 /***************************************************************************/
 
 int main() {
-	test_for_each();
-	test_out();
-	test_cast_to_char();
-	test_cast_from_char();
-	test_operators();
+    test_for_each();
+    test_iterators();
+    test_out();
+    test_cast_to_char();
+    test_cast_from_char();
+    test_operators();
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 /***************************************************************************/
